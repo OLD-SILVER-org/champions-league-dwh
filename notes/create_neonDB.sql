3137a779a00d7e35154b80ae2251c7f728584eab
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-02-27 00:34:28 +0700

    docs: add create_neonDB.sql

---
+++ notes/create_neonDB.sql
@@ -0,0 +1,52 @@
+CREATE TABLE match_details (
+    match_id TEXT,
+    season TEXT,
+    player_id TEXT,
+    shirt_number INT,
+    team_id TEXT,
+    goals INT,
+    own_goals INT,
+    yellow_cards INT,
+    red_card INT,
+    bench BOOLEAN
+);
+
+CREATE TABLE players (
+    player_id TEXT,
+    season TEXT,
+    name TEXT,
+    nation TEXT,
+    positions TEXT,
+    squad_id TEXT,
+    squad TEXT,
+    born INT
+);
+
+CREATE TABLE scores_and_fixtures (
+    id SERIAL,  -- ID tự tăng, không đặt PRIMARY KEY
+    match_id TEXT,
+    season TEXT,
+    round TEXT,
+    week INT,
+    day TEXT,
+    date DATE,
+    time TEXT,
+    home TEXT,
+    xg_home FLOAT,
+    score TEXT,
+    xg_away FLOAT,
+    away TEXT,
+    attendance TEXT,
+    venue TEXT,
+    referee TEXT,
+    match_report TEXT
+);
+
+CREATE TABLE squads (
+    squad_id TEXT,
+    season TEXT,
+    country TEXT,
+    name TEXT,
+    num_players INT,
+    matches_played INT
+);
