d18773021fa119224657984b10b116b86867fcb4
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-03-01 01:47:02 +0700

    feat: implement MatchDetails class for loading match details from CSV files

---
+++ tasks/load/staging/match_details_loader.py
@@ -0,0 +1,48 @@
+from neon_loader import NeonLoader
+import os
+
+
+class MatchDetails(NeonLoader):
+    def __init__(self, match_id=0):
+        super().__init__()
+        self.MATCH_DETAILS_LOCATION = str(
+            os.getenv("MATCH_DETAILS_LOCATION")).upper()
+        self.match_id = match_id
+
+    def load_newest_data(self):
+        self.load_data(self.match_id)
+        pass
+
+    def load_data(self, match_id):
+        """Main transform pipeline"""
+        path = self.get_transformed_path(match_id)
+        print(
+            f"🔄 DEBUG : newest file in {path} \n load to table : {self.MATCH_DETAILS_LOCATION} ")
+        columns = [
+            "match_id", "player_id", "shirt_number", "team_id",
+            "goals", "own_goals", "yellow_cards", "red_card", "bench"
+        ]
+
+        self.load_csv(path, self.TABLE_MATCH_DETAILS, columns=columns)
+        print(
+            f"✅  DEBUG : Load Done ")
+
+    def get_transformed_path(self, match_id):
+        """Load transformed data from file"""
+        path = os.path.join(
+            self.LV2_SAVE_PATH, self.MATCH_DETAILS_LOCATION, str(match_id))
+        if not os.path.exists(path):
+            print(f"⚠️ WARNING: Directory {path} does not exist!")
+        files = [f for f in os.listdir(path) if f.endswith(".csv")]
+        if not files:
+            return None  # No files found
+        latest_file = sorted(files, reverse=True)[0]
+        latest_file_path = os.path.join(path, latest_file)
+        # Read CSV and return DataFrame
+        return latest_file_path
+
+
+if __name__ == "__main__":
+    ml = MatchDetails()
+    ml.match_id = 19789895
+    ld = ml.load_newest_data()
