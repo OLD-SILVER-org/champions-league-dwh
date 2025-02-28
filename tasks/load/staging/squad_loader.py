e2329f3a3fd90149adaacf7183dc0e15e051ad8f
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-03-01 01:23:50 +0700

    feat: implement SquadLoader class for loading squad data from CSV files

---
+++ tasks/load/staging/squad_loader.py
@@ -0,0 +1,39 @@
+from neon_loader import NeonLoader
+import os
+
+
+class SquadLoader(NeonLoader):
+    def __init__(self):
+        super().__init__()
+        self.SQUADS_LOCATION = str(
+            os.getenv("SQUADS_LOCATION")).upper()
+
+    def load_data(self, season):
+        """Main transform pipeline"""
+        path = self.get_transformed_path(season)
+        print(
+            f"🔄 DEBUG : newest file in {path} \n load to table : {self.SQUADS_LOCATION} ")
+        columns = ["season", "nk", "country", "name",
+                   "number_of_player", "matches_played"]
+        self.load_csv(path, self.TABLE_SQUADS, columns=columns)
+        print(
+            f"✅  DEBUG : Load Done ")
+
+    def get_transformed_path(self, season):
+        """Load transformed data from file"""
+        path = os.path.join(
+            self.LV2_SAVE_PATH, self.SQUADS_LOCATION, str(season))
+        if not os.path.exists(path):
+            print(f"⚠️ WARNING: Directory {path} does not exist!")
+        files = [f for f in os.listdir(path) if f.endswith(".csv")]
+        if not files:
+            return None  # No files found
+        latest_file = sorted(files, reverse=True)[0]
+        latest_file_path = os.path.join(path, latest_file)
+        # Read CSV and return DataFrame
+        return latest_file_path
+
+
+if __name__ == "__main__":
+    ml = SquadLoader()
+    ld = ml.load_newest_data()
